e0c68f0c0cb26a4ecdc56fd253d5e2fdfd7206b7
= <itzsymbaa@outlook.com>
2020-11-05 21:51:56 +0000

    Switched graph library and added placeholder pie chart in AnalyticsActivity.kt

--- app/src/main/java/com/example/spudgmoneymanager/AnalyticsActivity.kt
+++ app/src/main/java/com/example/spudgmoneymanager/AnalyticsActivity.kt
@@ -1,31 +1,43 @@
 package com.example.spudgmoneymanager
 
 import android.content.Intent
+import android.graphics.Color
 import android.os.Bundle
+import android.view.WindowManager
+import android.widget.SeekBar
 import androidx.appcompat.app.AppCompatActivity
-import com.github.aachartmodel.aainfographics.aachartcreator.AAChartModel
-import com.github.aachartmodel.aainfographics.aachartcreator.AAChartType
-import com.github.aachartmodel.aainfographics.aachartcreator.AAChartView
-import com.github.aachartmodel.aainfographics.aachartcreator.AASeriesElement
+import com.github.mikephil.charting.animation.Easing
+import com.github.mikephil.charting.charts.PieChart
+import com.github.mikephil.charting.components.Legend
+import com.github.mikephil.charting.data.PieData
+import com.github.mikephil.charting.data.PieDataSet
+import com.github.mikephil.charting.data.PieEntry
+import com.github.mikephil.charting.formatter.PercentFormatter
+import com.github.mikephil.charting.utils.ColorTemplate
+import com.github.mikephil.charting.utils.MPPointF
 import kotlinx.android.synthetic.main.activity_analytics.*
 
 class AnalyticsActivity : AppCompatActivity() {
+
+    var rainFall: ArrayList<Float> = arrayListOf(98.8f, 123.8f, 161.6f)
+    var monthNames: ArrayList<String> = arrayListOf("First", "Second", "Third")
+    var entries: ArrayList<PieEntry> = ArrayList()
+    var categoryTitles: ArrayList<String> = ArrayList()
+    var categoryColours: ArrayList<Int> = ArrayList()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_analytics)
+
+        setupPieChart()
 
         back_to_trans_from_analytics.setOnClickListener {
             val intent = Intent(this, MainActivity::class.java)
             startActivity(intent)
         }
 
-        val aaChartView = findViewById<AAChartView>(R.id.aa_chart_view)
-
         val dbHandlerCategory = CategoriesHandler(this, null)
-        val dbHandlerTransaction = TransactionsHandler(this, null)
         var categories = dbHandlerCategory.getAllCategories()
-        var categoryTitles: ArrayList<String> = ArrayList()
-        var categoryColours: ArrayList<String> = ArrayList()
 
         for (category in categories) {
             categoryTitles.add(category.title)
@@ -33,34 +45,37 @@ class AnalyticsActivity : AppCompatActivity() {
 
         for (category in categories) {
             var intColor = category.colour.toInt()
-            categoryColours.add(java.lang.String.format("#%06X", 0xFFFFFF and intColor))
+            //categoryColours.add(java.lang.String.format("#%06X", 0xFFFFFF and intColor))
+            categoryColours.add(intColor)
         }
 
-        var graphElements: Array<AASeriesElement> = Array(categories.size) { AASeriesElement() }
-
-        for (i in 0 until categories.size) {
-            var totalForCategory = dbHandlerTransaction.getTransactionTotalForCategory(i + 1)
-            var categoryNameTotal = arrayOf(categoryTitles[i], totalForCategory)
-            graphElements[i] =
-                AASeriesElement().name(categoryTitles[i]).data(arrayOf(categoryNameTotal))
-                    .color(categoryColours[i])
-        }
-
-        val aaChartModel: AAChartModel = AAChartModel()
-            .chartType(AAChartType.Column)
-            .title("Transactions")
-            .subtitle("per category")
-            .backgroundColor("#FFFFFF")
-            .yAxisAllowDecimals(true)
-            .dataLabelsEnabled(true)
-            .xAxisLabelsEnabled(false)
-            .yAxisTitle("Total transactions")
-            .series(
-                graphElements
-            )
-
-        aaChartView.aa_drawChartWithChartModel(aaChartModel)
 
     }
 
+
+    private fun setupPieChart() {
+
+        for (i in 0 until 3) {
+            entries.add(PieEntry(rainFall[i], monthNames[i]))
+        }
+
+        var dataSet: PieDataSet = PieDataSet(entries, "Rainfall")
+        //dataSet.colors = categoryColours.toMutableList()
+        var data: PieData = PieData(dataSet)
+
+        var chart: PieChart = chart1
+        chart.data = data
+        chart.animateY(600)
+        chart.invalidate()
+
+    }
+
+
+
+
+
 }
+
+
+
+
