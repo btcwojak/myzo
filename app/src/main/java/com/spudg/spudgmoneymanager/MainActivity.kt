faf1d667cbb95fbee749ef0e629eb914fc1258ec
= <itzsymbaa@outlook.com>
2020-12-04 17:46:04 +0000

    Update add and edit categories and accounts to limit 1 line for title

--- app/src/main/java/com/spudg/spudgmoneymanager/MainActivity.kt
+++ app/src/main/java/com/spudg/spudgmoneymanager/MainActivity.kt
@@ -1,8 +1,10 @@
 package com.spudg.spudgmoneymanager
 
 import android.app.Dialog
+import android.content.Context
 import android.content.Intent
 import android.content.pm.PackageManager
+import android.content.res.Configuration
 import android.graphics.Color
 import android.graphics.drawable.ColorDrawable
 import android.os.Bundle
